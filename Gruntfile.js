3a4f814addf49a71cfdd0462be09fbab9cb40df9
fabloliv <fabloliv@outlook.com>
2013-08-01 02:50:26 -0300

    Adicionar prefixos no gradiente do tema 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
                 dest: '/home/fabriciolo2/public_html',
                 host: 'fabriciolo2@fabriciolo.com',
                 recursive: true,
-                syncDest: true
+                syncDest: false
             }
         }
     };
